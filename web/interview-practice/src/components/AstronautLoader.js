06e37485ba78d567fe6f0b51d1c9dfe01565e8a7
Ranjit-Singh-Dhunna <rs00dhunna@gmail.com>
2025-08-19 14:08:49 -0400

    loader 2 done

--- web/interview-practice/src/components/AstronautLoader.js
+++ web/interview-practice/src/components/AstronautLoader.js
@@ -39,13 +39,11 @@ const AstronautLoader = () => {
       </div>
       <div className="loader-bottom">
         <div className="loader-wrapper">
-          <span className="loader-letter">G</span>
-          <span className="loader-letter">e</span>
+          <span className="loader-letter">A</span>
           <span className="loader-letter">n</span>
-          <span className="loader-letter">e</span>
-          <span className="loader-letter">r</span>
           <span className="loader-letter">a</span>
-          <span className="loader-letter">t</span>
+          <span className="loader-letter">l</span>
+          <span className="loader-letter">y</span>
           <span className="loader-letter">i</span>
           <span className="loader-letter">n</span>
           <span className="loader-letter">g</span>
--- web/interview-practice/src/components/SpaceshipLoader.js
+++ web/interview-practice/src/components/SpaceshipLoader.js
@@ -21,6 +21,20 @@ const SpaceshipLoader = () => {
         <div className="cloud cloud4"></div>
         <div className="cloud cloud5"></div>
       </div>
+      <div className="card">
+        <div className="loader3">
+          <p>Analysing</p>
+          <div className="words">
+            <span className="word">Resume</span>
+            <span className="word">Links</span>
+            <span className="word">Answers</span>
+            <span className="word">Performance</span>
+            <span className="word">Concept</span>
+            {/* Duplicate first word for seamless loop */}
+            <span className="word">Resume</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
